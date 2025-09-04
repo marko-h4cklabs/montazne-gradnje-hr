0415147ce6b8aa24f03de8aec21cca38df943723
Marko Srnec <marko@h4cklabs.com>
2025-09-04 15:16:07 +0200

    changes2

--- src/components/ProductCategories.tsx
+++ src/components/ProductCategories.tsx
@@ -29,7 +29,7 @@ const ProductCategories = () => {
       <div className="container mx-auto px-4">
         <div className="text-center mb-16">
           <h2 className="text-4xl md:text-5xl font-bold mb-6 text-foreground">
-            Što sve radimo?
+            Čime se sve bavimo?
           </h2>
         </div>
         
